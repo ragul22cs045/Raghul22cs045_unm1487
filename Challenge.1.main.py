01a6f7198c99bb8f650cfbdf73004622653ec711
ragul22cs045 <150111844+ragul22cs045@users.noreply.github.com>
2023-11-09 13:37:49 +0530

    Challenge.1.main.py

---
+++ Challenge.1.main.py
@@ -0,0 +1,15 @@
+#1.program for factorial number
+"""
+formula = n(n-1)!
+ex:1! = 1*1
+"""
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+    
+number= int(input("ENTER A VALUE:"))
+result= fact_rec(number)
+
+print("The Factorial of {} is {}.".format(number,result))
